16126eb10bfe525a96703bd4bef8988b977e8852
monoditrium <104584805+monoditrium@users.noreply.github.com>
2024-02-10 23:58:44 +0700

    Add files via upload

--- githubRepo.py
+++ githubRepo.py
@@ -2,7 +2,7 @@ from ast import main
 from github import Github
 import csv
 
-token = "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" #paste your token here
+token = "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" #paste your token here
 
 g = Github(token)
 
@@ -14,17 +14,11 @@ def repo_search(keyword):
     file = open("github_Repo_Output.csv", "w", newline = '', encoding='utf-8')
     writer = csv.writer(file, quoting=csv.QUOTE_MINIMAL)
     writer.writerow(['Full Name', 'HTML URL','Description'])
-    # print(f'Full Name HTML URL Description')
 
     print(f"Found {result.totalCount} repo(s)")
     print("outputting into a csv file...")
     for repo in result:
         writer.writerow([repo.full_name, repo.html_url, repo.description])
-        # print(f'{repo.full_name}    {repo.description}    {repo.html_url}')
-        # print(repo.description)
-        # print(repo.html_url)
-        # print()
-    
     print("result output complete")
 
 def main():
@@ -37,7 +31,5 @@ def main():
     repo_search(keywords)
     print("program executed")
 
-
 if __name__ == '__main__':
     main()
-# print(g.get_user().get_repos())
